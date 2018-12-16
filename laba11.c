4a392e2590bf9f06a74cbff3dd2d5a4274febf96
Amirsz01 <43276827+Amirsz01@users.noreply.github.com>
2018-12-16 23:48:48 +0300

    Update laba11.c

--- laba11.c
+++ laba11.c
@@ -7,22 +7,23 @@
 #define MAXLEN 1000
 void main(void)
 {
-	HANDLE hStdout; // дескриптор консольного окна
-	FILE *fp; // входной файл
-	char line[MAXLEN]; // строчка из файла
-	char word[MAXLEN]; // слово
+	HANDLE hStdout;
+	FILE *fp;
+	char line[MAXLEN]; 
+	char word[MAXLEN]; 
 	int i;
-	int flag; // флаг слово/разделитель
-	int prev_flag; // предыдущее значение флага
-	char *ptr; // указатель на строчку
-	WORD foregroundColor0; // цвет слов
-	WORD textAttribute; // атрибут текста - цвет символа и фона
-	// Получить стандартный дескриптор
+	int flag;
+	int prev_flag; 
+	int flag_kom1 = 0;
+	int flag_kom2 = 0;
+	char *ptr;
+	WORD foregroundColor0;
+	WORD textAttribute;
 	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 	foregroundColor0 = FOREGROUND_INTENSITY | FOREGROUND_BLUE | FOREGROUND_GREEN;
 	fp = fopen("test.txt", "rt");
 	if (fp == NULL)
-		return; // ошибка при открытии файла
+		return; 
 	while (!feof(fp))
 	{
 		ptr = fgets(line, MAXLEN, fp);
@@ -33,15 +34,15 @@ void main(void)
 		word[0] = '\0';
 		while (*ptr != 0)
 		{
-			prev_flag = flag; // запомнить флаг
-			if (*ptr == ' ' || *ptr == ',' || *ptr == '.' ||*ptr == '\n' || *ptr == '[' || *ptr == ']')
+			prev_flag = flag; 
+			if (*ptr == ' ' || *ptr == ',' || *ptr == '.' || *ptr == '\n' || *ptr == '[' || *ptr == ']' || *ptr == '/' || *ptr == '*')
 				flag = NOT_THE_LETTER;
 			else
 				flag = LETTER;
 			if (flag != prev_flag)
 			{
 				word[i] = '\0';
-				if (*ptr == '[') 
+				if (*ptr == '[' && !flag_kom1 && !flag_kom2)
 				{
 					textAttribute = foregroundColor0;
 					SetConsoleTextAttribute(hStdout, textAttribute);
@@ -50,15 +51,31 @@ void main(void)
 				{
 					SetConsoleTextAttribute(hStdout, 7);
 				}
-				printf("%s", word); // вывести слово
-				i = 0; // начать новое слово
+				printf("%s", word);
+				i = 0; 
 			}
-			word[i++] = *ptr++; // запомнить символ
+			if (*ptr == '/' && *(ptr + 1) == '/')
+			{
+				flag_kom1 = 1;
+			}
+			if (*ptr == '/' && *(ptr + 1) == '*')
+			{
+				flag_kom2 = 1;
+			}
+			if (*ptr == '*' && *(ptr + 1) == '/')
+			{
+				flag_kom2 = 0;
+			}
+			if (*ptr == '\n')
+			{
+				flag_kom1 = 0;
+			}
+			word[i++] = *ptr++;
 		}
 		if (i != 0)
 		{
-			word[i] = '\0'; // закрыть слово
-			printf("%s", word); // вывести слово
+			word[i] = '\0';
+			printf("%s", word);
 		}
 	}
 	printf("\n");
